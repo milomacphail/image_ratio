9f9d900e9baf49c2596a0f3f1ad118f7283739a1
milomacphail <milomacphail@gmail.com>
2019-04-06 20:03:35 -0400

    Implemented landscape/portrait

---
+++ landscape_portrait/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace landscape_portrait
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an image height.");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter an image width.");
+            int width = int.Parse(Console.ReadLine());
+
+            if (height > width)
+            {
+                Console.WriteLine("Portrait");
+            } else if (width > height) {
+                Console.WriteLine("Landscape");
+            } else if (width == height)
+            {
+                Console.WriteLine("Square");
+            }
+        }
+    }
+}
